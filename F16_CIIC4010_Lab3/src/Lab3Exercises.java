412a2d214b1716acd9f5c3391176356340e7bb9e
4010b1 <4010b1@chironja.ece.uprm.edu>
2016-08-31 10:05:57 -0400

    Added an oval that stays on the center of the frame.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -1,11 +1,17 @@
 import javax.swing.JFrame;
 
-public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+public class Lab3Exercises 
+{
+	public static void main(String[] args) 
+	{
+		JFrame axvielBenitez = new JFrame("Hello, world");
+		axvielBenitez.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		axvielBenitez.setLocation(540, 412);
+		axvielBenitez.setSize(200, 200);
+		
+		MyPanelClass myPanel = new MyPanelClass();
+		axvielBenitez.getContentPane().add(myPanel);
+		
+		axvielBenitez.setVisible(true);
 	}
 }
---
+++ F16_CIIC4010_Lab3/src/MyPanelClass.java
@@ -0,0 +1,96 @@
+import java.awt.Color;
+
+import java.awt.Graphics;
+
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+
+
+
+public class MyPanelClass extends JPanel 
+
+{
+
+	/**
+
+	 * 
+
+	 */
+
+	private static final long serialVersionUID = 7057541440811488699L;
+
+
+
+	public void paintComponent(Graphics g) 
+
+	{
+
+		super.paintComponent(g);
+
+
+
+		//Compute interior coordinates
+
+		Insets myInsets = this.getInsets();
+
+		int x1 = myInsets.left;
+
+		int y1 = myInsets.top;
+
+		int x2 = getWidth() - myInsets.right - 1;
+
+		int y2 = getHeight() - myInsets.bottom - 1;
+
+		int width = x2 - x1;
+
+		int height = y2 - y1;
+
+
+
+		//Paint the background
+
+		g.setColor(Color.RED);
+
+		g.fillRect(x1, y1, width+1, height+1);
+
+
+		//        //Draw a border
+
+		//        g.setColor(Color.YELLOW);
+
+		//        g.drawRect(x1, y1, width, height);
+
+		//        
+
+		//        //Draw an inside border
+
+		//        g.setColor(Color.BLACK);
+
+		//        g.drawRect(x1+5, y1+5, width-10, height-10);
+
+		//        
+
+		//        //Draw a line
+
+		//        g.setColor(Color.WHITE);
+
+		//        g.drawLine(x1, y1, x2, y2);
+
+		//        
+
+		//        //Draw another line
+
+		//        g.setColor(Color.BLUE);
+
+		//        g.drawLine(x1, y1+getHeight(), x2, y2-getHeight());
+
+
+		g.setColor(Color.LIGHT_GRAY);
+
+		g.fillOval(getWidth()/2-22, getHeight()/2-22, 55, 55);
+		//g.fillOval((x2-(55/2))/2, (y2-(55/2))/2, 55, 55);
+
+	}
+
+}
